679511d1dc6c20ccd70060cc63015a3b5b5df464
Jothirmoy Sarker Shuvo <jothirmoysarker118@gmail.com>
2024-11-24 13:27:15 +0600

    Update handler.php

--- git Lab Exam/handler.php
+++ git Lab Exam/handler.php
@@ -1,6 +1,22 @@
 <?php
+session_start();
+
+$valid_email = "user@gmail.com";
+$valid_password = "123";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    echo "Name: " . $name;
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    echo "email: " . $email;
+    echo "password: " . $password;
+
+
+    if ($email == $valid_email && $password == $valid_password) {
+        $_SESSION['email'] = $email;
+        header("Location: home.html");
+        exit();
+    } else {
+        echo "Invalid email or password.";
+    }
 }
 ?>
